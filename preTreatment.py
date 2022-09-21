2a3e200edfcb686065cee1b28ee090fbe85e3c72
Your kaminosekai54 <culpin.alexis@laposte.net>
2022-09-21 19:38:21 +0200

    forgotteng pretreatment file

---
+++ preTreatment.py
@@ -0,0 +1,88 @@
+from collections import Counter
+from settings import *
+import  os
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+
+# function preTreatmentFasta
+# this function pretreat the raw fasta file in order 
+# to filter sequence of a minimal length
+# and that contain both of  the start and end patern
+# this function also provide a log file that indicate the amount of sequence filtered and the amount of sequence with one or the other patern
+# @param,
+# @fastaFile, The raw fasta file to treat 
+# @sourceFolder, the source folder where to find fasta file, 
+# @destinationFolder, the destination folder where to write the pre-treated fasta
+def preTreatmentFasta(fastaFile, sourceFolder = settings["pretreatment"]["sourceFolder"], destinationFolder = settings["pretreatment"]["destinationFolder"]):
+    
+    refSeq= SeqRecord(Seq(settings["refSeqSequence"]), id = settings["refSeqId"], name = "", description= "")
+    seqLengthList = [len(refSeq.seq)]
+    seqList = [refSeq]
+    
+    EndMotifOnly = 0
+    BeginningMotifOnly = 0
+    BothMotif = 0
+    
+    file_number = fastaFile[fastaFile.find("T0"):fastaFile.find("T0")+3]
+    print(file_number)
+
+    for record in SeqIO.parse(sourceFolder+ fastaFile, "fasta"):
+        haveBeginningMotif = False
+        haveEndMotif= False
+        
+        seq = str(record.seq)
+        if settings["pretreatment"]["pre_treatment_start_1"] in seq and int(file_number[-1]) <=5: 
+            haveBeginningMotif=True
+            seq = seq[seq.find(settings["pretreatment"]["pre_treatment_start_1"]) -5:]
+
+        if settings["pretreatment"]["pre_treatment_start_2"] in seq and int(file_number[-1]) >5: 
+            haveBeginningMotif=True
+            seq = seq[seq.find(settings["pretreatment"]["pre_treatment_start_2"])  -5:]
+
+        if settings["pretreatment"]["pre_treatment_end_1"] in seq and int(file_number[-1]) <=5: 
+            haveEndMotif=True
+            seq = seq[:seq.rfind(settings["pretreatment"]["pre_treatment_end_1"]) + len(settings["pretreatment"]["pre_treatment_end_1"])]
+
+        if settings["pretreatment"]["pre_treatment_end_2"] in seq and int(file_number[-1]) > 5: 
+            haveEndMotif=True
+            seq = seq[:seq.rfind(settings["pretreatment"]["pre_treatment_end_2"]) + len(settings["pretreatment"]["pre_treatment_end_2"])]
+        
+        record.seq = Seq(seq)
+        
+        if len(seq) > settings["pretreatment"]["length_threshold"] and int(file_number[-1]) <= 5:
+            if haveBeginningMotif and not haveEndMotif: BeginningMotifOnly+=1
+            if not haveBeginningMotif and haveEndMotif: EndMotifOnly+=1
+            if haveEndMotif and haveBeginningMotif: 
+                BothMotif +=1
+                seqList.append(record)
+                seqLengthList.append(len(seq))
+
+        elif len(seq) > settings["pretreatment"]["length_threshold"] and int(file_number[-1]) > 5:
+            if haveBeginningMotif and not haveEndMotif: BeginningMotifOnly+=1
+            if not haveBeginningMotif and haveEndMotif: EndMotifOnly+=1 
+            if haveBeginningMotif: 
+                BothMotif +=1
+                seqList.append(record)
+                seqLengthList.append(len(seq))
+
+    log = "The file " + fastaFile + " after pre-treatment has " + str(len(seqList)) + " sequences \n"
+    log+= "including : \n" + str(BeginningMotifOnly) + " sequences have the Beginning motif only \n"
+    log+= str(EndMotifOnly) + " sequences have the End motif only \n"
+    log+= str(BothMotif) + " sequences have both the Beginning and End motif. \n"
+    log+= "The Beginning motif is %s and the End motif is %s." %(settings["pretreatment"]["pre_treatment_start_1"], settings["pretreatment"]["pre_treatment_end_1"])
+    log+= "-------------\n"
+
+    if int(file_number[-1]) <=5:
+        destinationFolder += '/DirectedEvolutionData/'
+    elif int(file_number[-1]) > 5:
+        destinationFolder += '/MutagenesisData/'
+
+    if not os.path.isdir(destinationFolder) : os.makedirs(destinationFolder)
+    if not os.path.isfile(destinationFolder+"log.txt"): logFile = open(destinationFolder+ "log.txt", "w").close()
+    with open(destinationFolder+"log.txt", "a") as logFile:
+        logFile.write(log)
+
+    print("write fasta")
+    SeqIO.write(seqList, destinationFolder + fastaFile.replace(".fasta", "_pre-treated.fasta"), "fasta-2line")
+    return seqLengthList
