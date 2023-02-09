5df14e78be10b6d3a0f834ce160109baf8fa489f
Your kaminosekai54 <culpin.alexis@laposte.net>
2023-02-09 15:00:20 +0100

    a

--- fastqTofasta.py
+++ fastqTofasta.py
@@ -18,7 +18,7 @@ def fastqToFasta(fastqFile, sourceFolder = "raw_data/", destinationFolder = "fas
 def convertAll(path = "raw_data/"):
     sourceFolder = str(input("please enter the path to the source folder containing the fastQ files"))
     destinationFolder = str(input("please enter the path where to store the converted fasta"))
-    fileList = [file for file in os.listdir(path) if file.endswith(".fastq.gz")]
+    fileList = [file for file in os.listdir(sourceFolder) if file.endswith(".fastq.gz")]
     for file in fileList:
         fastqToFasta(file, sourceFolder=sourceFolder, destinationFolder=destinationFolder)
 
